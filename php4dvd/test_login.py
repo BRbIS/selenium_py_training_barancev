70505daea080e5bf13fc04a8bef614f6b4594852
a.gorgoma@securitycode.ru <a.gorgoma@securitycode.ru>
2015-07-29 21:39:18 +0300

    create def's login and logout

--- php4dvd/test_login.py
+++ php4dvd/test_login.py
@@ -6,13 +6,18 @@ from selenium_fixture import driver
 
 base_url = "http://172.17.10.17/"
 
+def logout(driver):
+    driver.find_element_by_link_text("Log out").click()
+    driver.switch_to_alert().accept()
 
-def test_login(driver):
-    driver.get(base_url + "/php4dvd/")
+def login(driver):
     driver.find_element_by_id("username").clear()
     driver.find_element_by_id("username").send_keys("admin")
     driver.find_element_by_name("password").clear()
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("submit").click()
-    driver.find_element_by_link_text("Log out").click()
-    driver.switch_to_alert().accept()
+
+def test_login(driver):
+    driver.get(base_url + "/php4dvd/")
+    login(driver)
+    logout(driver)
