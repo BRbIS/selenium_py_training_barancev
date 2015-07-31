7c0b5c084d3736ab6f43fbc278de0f9e6665aaac
a.gorgoma@securitycode.ru <a.gorgoma@securitycode.ru>
2015-07-31 14:07:21 +0300

    in step12 remote firefox now work need to fix chrome and install Ie

--- php4dvd/model/application.py
+++ php4dvd/model/application.py
@@ -9,12 +9,13 @@ from selenium.webdriver.support.expected_conditions import *
 
 class Application(object):
 
-    def __init__(self, driver):
+    def __init__(self, driver, base_url):
         self.driver = driver
+        self.base_url = base_url
         self.wait = WebDriverWait(driver, 10)
 
     def go_to_home_page(self):
-        self.driver.get(base_url + "/php4dvd/")
+        self.driver.get(self.base_url)
 
     def logout(self):
         driver = self.driver
