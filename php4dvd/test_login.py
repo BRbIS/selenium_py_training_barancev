716bee4518cb17a5a92ffd123118cf4a5dc06386
a.gorgoma@securitycode.ru <a.gorgoma@securitycode.ru>
2015-07-31 21:18:38 +0300

    step20 tests self-care

--- php4dvd/test_login.py
+++ php4dvd/test_login.py
@@ -5,12 +5,12 @@ from model.user import User
 
 
 def test_login_with_invalid_credentials(app):
+    app.ensure_logout()
     app.login(User.random())
     assert app.is_not_logged_in()
 
 
 def test_login(app):
+    app.ensure_logout()
     app.login(User.Admin())
     assert app.is_logged_in()
-    app.logout()
-    assert app.is_not_logged_in()
